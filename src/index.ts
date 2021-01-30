7491df503f7c8df7ea30dd3455add47e5bb552e2
MssKnd <m-kondo@safie.jp>
2021-01-30 20:45:50 +0900

    hls

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import Hls from 'hls.js';
+
 class HlsPlayableVideoElement extends HTMLVideoElement {
 
   constructor() {
     super();
-    console.log('constructor')
+    console.log(Hls)
   }
 
   connectedCallback() {
@@ -14,7 +16,17 @@ class HlsPlayableVideoElement extends HTMLVideoElement {
   }
 
   attributeChangedCallback(name: string, oldValue: string, newValue: string) {
-    console.log(name, ' is change to ', newValue);
+    // console.log(name, ' is change to ', newValue);
+    if (
+      name === 'src' &&
+      newValue.endsWith('.m3u8') &&
+      !this.canPlayType('application/vnd.apple.mpegurl')
+    ) {
+      console.log('aaaaaaa')
+      const hls = new Hls();
+      hls.loadSource(newValue);
+      hls.attachMedia(this);
+    }
   }
 }
 
