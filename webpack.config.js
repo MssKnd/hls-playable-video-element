99c6ba8840417856b416c0dd94f306efa88aafd1
MssKnd <m-kondo@safie.jp>
2021-01-31 03:17:52 +0900

    webpack --watach

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/index.ts',
+  cache: true,
   output: {
     path: path.join(__dirname, "dist"),
     filename: "index.js"
