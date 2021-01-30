4c83d010f821607304152d10bbf1f39d8d32c5ac
MssKnd <m-kondo@safie.jp>
2021-01-30 17:03:31 +0900

    define customElements

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-function hello(name: string): string {
-  return `Hello, ${name}!`;
+class HlsPlayableVideoElement extends HTMLVideoElement {
+
+  constructor() {
+    super();
+    console.log('constructor')
+  }
+
+  connectedCallback() {
+    console.log('connectedCallback');
+  }
+
+  static get observedAttributes() {
+    return ['src'];
+  }
+
+  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+    console.log(name, ' is change to ', newValue);
+  }
 }
 
-console.log(hello("World"));
+customElements.define('hls-playable-video', HlsPlayableVideoElement, { extends: 'video' });
